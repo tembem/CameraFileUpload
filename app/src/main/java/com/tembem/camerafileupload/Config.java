b008cb87da06713995daa4e4db75b4f316f0d05f
Jimmy Chandra <jim@tembem.com>
2016-07-22 16:06:31 +0700

    Initial commit

---
+++ app/src/main/java/com/tembem/camerafileupload/Config.java
@@ -0,0 +1,11 @@
+package com.tembem.camerafileupload;
+
+/**
+ * Created by jim on 7/22/16.
+ */
+
+public class Config {
+    public static final String FILE_UPLOAD_URL = "http://192.168.1.3/androidfileupload/upload.php";
+
+    public static final String IMAGE_DIRECTORY_NAME = "files";
+}
